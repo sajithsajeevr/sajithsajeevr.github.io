a839c5500449a5f2faa8bd9cb5db1ecbb54011f8
Sajith Sajeev R <sajithsajeevr@Sajiths-MacBook-Air.local>
2023-09-20 21:20:10 +0100

    first

---
+++ precache-manifest.9d4d94bc14579474fb2fc8da38685404.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ace1c4c1e13a542f1032992e49cb7aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f588178ba2f3348dc80a",
+    "url": "/static/css/2.7aa6f31f.chunk.css"
+  },
+  {
+    "revision": "568993cb8d0d671773b6",
+    "url": "/static/css/main.99dd466d.chunk.css"
+  },
+  {
+    "revision": "f588178ba2f3348dc80a",
+    "url": "/static/js/2.60ac3c41.chunk.js"
+  },
+  {
+    "revision": "568993cb8d0d671773b6",
+    "url": "/static/js/main.30bc5e79.chunk.js"
+  },
+  {
+    "revision": "09b28f0dc63743cf419f",
+    "url": "/static/js/runtime-main.49d4abb3.js"
+  },
+  {
+    "revision": "6e479205a4538bab60f8ea51ac43d432",
+    "url": "/static/media/img1.6e479205.svg"
+  },
+  {
+    "revision": "2c8907dc7b8a36fc1e77e0058b66df6a",
+    "url": "/static/media/img2.2c8907dc.svg"
+  },
+  {
+    "revision": "8539034c12e8ba7f5a83ec39c3a59644",
+    "url": "/static/media/img3.8539034c.svg"
+  },
+  {
+    "revision": "dc7e5976cb3f4fd13a3e51918a98e7bc",
+    "url": "/static/media/img4.dc7e5976.svg"
+  },
+  {
+    "revision": "dd2288fc6be04bcbf8fda8efc830ea0f",
+    "url": "/static/media/img5.dd2288fc.svg"
+  },
+  {
+    "revision": "2b98972b6e250dcc5570c19d94bcaf23",
+    "url": "/static/media/img6.2b98972b.svg"
+  }
+]);
